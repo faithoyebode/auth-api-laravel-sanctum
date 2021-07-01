43c986cae28085bfc99a0b0e6d18727490aa16d8
Faith <oyebodefaith3@gmail.com>
2021-07-01 18:30:56 +0100

    feat(products): protection of  products routes that needs authentication created project setup; installed sanctum and configured it; created product routes and their respective methods under the ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required'
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if(is_null($product)){
+            return response()->json([
+                'status' => 'error',
+                'data' => '{}',
+                'message' => 'No record found'
+            ], 404);
+        }else{
+            return $product;
+        }
+         
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if(is_null($product)){
+            return response()->json([
+                'status' => 'error',
+                'data' => '{}',
+                'message' => 'The product that you want to update does not exist'
+            ], 404);
+        }
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+
+    /**
+     * Search for a name.
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        $products = Product::where('name', 'LIKE', '%'.$name.'%')->get();
+        if(count($products) == 0){
+            return response()->json([
+                'status' => 'error',
+                'data' => [],
+                'message' => 'There is no product name that matches your search string'
+            ], 404);
+        }
+        return $products;
+    }
+}
